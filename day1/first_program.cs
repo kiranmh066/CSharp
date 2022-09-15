4249553106908a7700d5b0b01a6c050e5e240af8
kiranmh066 <94098639+kiranmh066@users.noreply.github.com>
2022-09-15 14:07:24 +0530

    Create first_program.cs

---
+++ day1/first_program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_training
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a+ b;
+            Console.Write("output:"+ c);
+            Console.Write("Completed");*/
+            
+            for(int i = 0; i < 4; i++)
+            {
+
+            }
+            Console.Read();
+        }
+    }
+}
