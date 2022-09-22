3e28158edd17cf406ef47a0429743993ad9436b2
kiranmh066 <94098639+kiranmh066@users.noreply.github.com>
2022-09-22 17:55:42 +0530

    Create Admin.cs

---
+++ LibraryManagemnt/Entity/Admin.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementEntity
+{
+    public class Admin
+    {
+        public string AdminEmail { get; set; }
+
+        public int AdminId { get; set; }
+
+        public string AdminName { get; set; }
+
+        public string AdminPassword { get; set; }
+
+        public Admin(string adminEmail,int adminId,string adminName,string adminPassword)
+        {
+            this.AdminEmail=adminEmail;
+            this.AdminId=adminId;
+            this.AdminName=adminName;
+            this.AdminPassword=adminPassword;
+        }
+
+        
+
+    }
+}
+
+namespace LibraryManagementEntity
+{
+    public class Admin
+    {
+        public string AdminEmail { get; set; }
+
+        public int AdminId { get; set; }
+
+        public string AdminName { get; set; }
+
+        public string AdminPassword { get; set; }
+
+        public Admin(string adminEmail,int adminId,string adminName,string adminPassword)
+        {
+            this.AdminEmail=adminEmail;
+            this.AdminId=adminId;
+            this.AdminName=adminName;
+            this.AdminPassword=adminPassword;
+        }
+
+        
+
+    }
+}
+ad
