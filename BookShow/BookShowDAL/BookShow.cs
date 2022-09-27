3a9a9f8a4c40440b95a2c8e98ac0168b8dfb4703
kiranmh066 <94098639+kiranmh066@users.noreply.github.com>
2022-09-27 18:00:53 +0530

    Add files via upload

---
+++ BookShow/BookShowDAL/BookShow.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using BookShowEntity;
+
+namespace BookShowDAL
+{
+    public class BookShowDbContext:DbContext
+    {
+        public DbSet<Movie> movies { get; set; }
+        public DbSet<Theatre> theatre { get; set; }
+        public DbSet<ShowTiming> showTiming { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer("Data Source=VDC01LTC2164; Initial Catalog = BookShow; Integrated Security=True;");
+        }
+    }
+}
